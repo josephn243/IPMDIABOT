02f100f6240ada2a89c1337103a0f6c075a496cf
josephn243 <49919244+josephn243@users.noreply.github.com>
2021-02-04 14:06:31 -0800

    Updates to apply command

--- moderation/apply.js
+++ moderation/apply.js
@@ -9,7 +9,6 @@ module.exports = {
             .setColor('#FFFF00')
             .setTitle( `Welcome to ItsDuckingLit`)
             .setDescription( ` To apply, post your RealmEye link below. If we have not gotten back to you in a timely manner, feel free to DM any <@&702976781228965982> , <@&701944958680039485> , or <@&701944952464343042>  when you're online or ping them in <#701701668198285325> `)
-            .setFooter('© IPMDIABOD', 'https://i.imgur.com/34hsUpc.png');
         let messageEmbed = await message.channel.send(embed);
     }
 }
